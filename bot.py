7b02f275d2aa1acbe194fe0585de59d6a25d0c63
Gilles Velleneuve Trindade Silvano <gillesvtsilvano@gmail.com>
2015-03-12 01:08:13 -0300

    List files.

--- bot.py
+++ bot.py
@@ -8,13 +8,49 @@ br = mechanize.Browser()
 
 br.open('https://' + domain)
 
-br.form.name = 'https://cloud.iip.ufrn.br/index.php/apps/files/?dir=%2F'
+#br.form.name = 'https://cloud.iip.ufrn.br/index.php/apps/files/?dir=%2F'
 br.select_form(name='login')
 br.form['user'] = 'registration'
 br.form['password'] = '123'
 
-#print br.form
+br.submit()
 
+#for f in br.forms():
+#	print f
+
+#br.form.pop()
+
+for i in br.response().read().split('\n'):
+	if 'data-requesttoken' in i:
+		token = i.split('=')[2][1:-2]
+
+br.select_form(nr=0)
+
+br.form.controls.pop()
+br.form.new_control('text', 'Accept', {})
+br.form['Accept'] = '*/*'
+br.form.new_control('text', 'requesttoken', {})
+br.form['requesttoken'] = token
+br.form.new_control('text', 'Refer', {})
+br.form['Refer'] = 'https://cloud.iip.ufrn.br/index.php/app/files/'
+br.form.new_control('text', 'X-Requested-With', {})
+br.form['X-Requested-With'] = 'XMLHttpRequest'
+#br.form.new_control('text', 'dir', {})
+#br.form['dir'] = u'/'
+#br.form.new_control('text', 'foldername', {})
+#br.form['foldername'] = 'PACHECO'
+br.form.action = 'https://cloud.iip.ufrn.br/index.php/apps/files/ajax/list.php?dir=%2F&soft=name&sortdirection=asc'
+br.form.method = 'GET'
+br.form.new_control('text', 'dir', {})
+br.form['dir'] = u'/'
+
+br.submit()
+
+for i in br.response().read().split('\n'):
+	if '\"data\":' in i:
+		print i
+
+"""
 for i in br.response().read().split('\n'):
 	if 'data-requesttoken' in i:
 		token = i.split('=')[2][1:-2]
@@ -45,7 +81,7 @@ for line in br.response().read().split('\n'):
 
 #br.submit()
 
-"""
+
 for i in br.response().read().split('\n'):
 	if 'data-requesttoken' in i:
 		token = i.split('=')[2][1:-2]
---
+++ oclist.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+s={"data":{"directory":"\/","files":[{"id":"655005","parentId":"655003","date":"March  5, 2015 16:21","mtime":1425572475000,"icon":"\/core\/img\/filetypes\/folder.svg","name":"documents","permissions":31,"mimetype":"httpd\/unix-directory","size":"23383","type":"dir","etag":"54f8827f8f180"},{"id":"655007","parentId":"655003","date":"March  5, 2015 16:21","mtime":1425572476000,"icon":"\/core\/img\/filetypes\/folder.svg","name":"music","permissions":31,"mimetype":"httpd\/unix-directory","size":"3764804","type":"dir","etag":"54f8827f8f4e8"},{"id":"655009","parentId":"655003","date":"March  5, 2015 16:21","mtime":1425572478000,"icon":"\/core\/img\/filetypes\/folder.svg","name":"photos","permissions":31,"mimetype":"httpd\/unix-directory","size":"678556","type":"dir","etag":"54f8827f8f86e"},{"id":"714167","parentId":"655003","date":"March  9, 2015 19:51","mtime":1425930688000,"icon":"\/core\/img\/filetypes\/folder.svg","name":"testewal","permissions":31,"mimetype":"httpd\/unix-directory","size":"0","type":"dir","etag":"54fdf9c0f2129"},{"id":"714175","parentId":"655003","date":"March  9, 2015 19:54","mtime":1425930853000,"icon":"\/core\/img\/filetypes\/folder.svg","name":"testewal2","permissions":31,"mimetype":"httpd\/unix-directory","size":"823614","type":"dir","etag":"54fdfa65e5da9"},{"id":"655004","parentId":"655003","date":"March  5, 2015 16:21","mtime":1425572475000,"icon":"\/core\/img\/filetypes\/application-pdf.svg","name":"ownCloudUserManual.pdf","permissions":27,"mimetype":"application\/pdf","size":"1761471","type":"file","etag":"54f8827f8ed61"}],"permissions":31},"status":"success"}
+a
+s
+s['data']
+s['data']['name']
+for i in s['data']:
+    print i
+    
+for i in s['data']:
+    print i['files']
+    
+s['data']['files']
+s['data']['files']['name']
+s['data']['files']
+s['data']['files'](0)
+s['data']['files']
+s['data']['files'][0]
+s['data']['files'][1]
+s['data']['files'][2]
+for i in s['data']['files']:
+    print i
+    
+for i in s['data']['files']:
+    print i['name']
+    
+for i in s['data']['files']:
+    print i['name'], i['type']
+    
+for i in s['data']['files']:
+    print i['name'] + '\t' + i['type']
+    
+for i in s['data']['files']:
+    print i['type'] + '\t' + i['name']
+    
+get_ipython().magic(u'save list_owncloud')
+get_ipython().magic(u'save')
+get_ipython().magic(u'save help')
+get_ipython().magic(u'save')
+get_ipython().magic(u'save %help')
+get_ipython().magic(u'save oclist 1-26')
